f9a10973d1e6133fd5369a44cc277677883ffe30
medawarsi <smedawar@borninteractive.com>
2025-04-06 08:39:05 +0300

    storer finished

--- ecomm-api/storer/storer_mysql.go
+++ ecomm-api/storer/storer_mysql.go
@@ -41,8 +41,8 @@ func (ms *MySQLStorer) GetProduct(ctx context.Context, id int64) (*Product, erro
 	return &p, nil
 }
 
-func (ms *MySQLStorer) ListProducts(ctx context.Context) ([]*Product, error) {
-	var products []*Product
+func (ms *MySQLStorer) ListProducts(ctx context.Context) ([]Product, error) {
+	var products []Product
 	err := ms.db.SelectContext(ctx, &products, "SELECT * FROM products")
 	if err != nil {
 		return nil, fmt.Errorf("error listing products: %w", err)
@@ -65,6 +65,152 @@ func (ms *MySQLStorer) DeleteProduct(ctx context.Context, id int64) error {
 
 	if err != nil {
 		return fmt.Errorf("error deleting product: %w", err)
+	}
+
+	return nil
+}
+
+func (ms *MySQLStorer) CreateOrder(ctx context.Context, o *Order) (*Order, error) {
+	err := ms.execTx(ctx, func(tx *sqlx.Tx) error {
+		order, err := createOrder(ctx, tx, o)
+
+		if err != nil {
+			return fmt.Errorf("error creating order: %w", err)
+		}
+
+		for _, oi := range o.Items {
+			oi.OrderID = order.ID
+			err = createOrderItem(ctx, tx, oi)
+			if err != nil {
+				return fmt.Errorf("error creating order item: %w", err)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error creating order: %w", err)
+	}
+
+	return o, nil
+}
+
+func createOrder(ctx context.Context, tx *sqlx.Tx, o *Order) (*Order, error) {
+	res, err := tx.NamedExecContext(ctx, "INSERT INTO orders (payment_method, tax_price, shipping_price, total_price, user_id) VALUES (:payment_method, :tax_price, :shipping_price, :total_price, :user_id)", o)
+	if err != nil {
+		return nil, fmt.Errorf("error inserting order: %w", err)
+	}
+
+	id, err := res.LastInsertId()
+
+	if err != nil {
+		return nil, fmt.Errorf("error getting last insert ID: %w", err)
+	}
+
+	o.ID = id
+	return o, nil
+}
+
+func createOrderItem(ctx context.Context, tx *sqlx.Tx, o OrderItem) error {
+	res, err := tx.NamedExecContext(ctx, "INSERT INTO order_items (name, quantity, image, price, product_id, order_id) VALUES (:name, :quantity, :image, :price, :product_id, :order_id)", o)
+	if err != nil {
+		return fmt.Errorf("error insertin gorder item: %w", err)
+	}
+
+	id, err := res.LastInsertId()
+
+	if err != nil {
+		return fmt.Errorf("error getting last insert ID: %w", err)
+	}
+
+	o.ID = id
+
+	return nil
+}
+
+func (ms *MySQLStorer) GetOrder(ctx context.Context, id int64) (*Order, error) {
+	var o Order
+	err := ms.db.GetContext(ctx, &o, "SELECT * FROM orders WHERE id=?", id)
+	if err != nil {
+		return nil, fmt.Errorf("error getting order: %w", err)
+	}
+
+	var items []OrderItem
+
+	err = ms.db.SelectContext(ctx, &items, "SELECt * FROM order_items WHERE order_id=?", id)
+
+	if err != nil {
+		return nil, fmt.Errorf("error getting order items: %w", err)
+	}
+
+	o.Items = items
+	return &o, nil
+}
+
+func (ms *MySQLStorer) ListOrders(ctx context.Context) ([]Order, error) {
+	var orders []Order
+	err := ms.db.SelectContext(ctx, &orders, "SELECt * FROM orders")
+
+	if err != nil {
+		return nil, fmt.Errorf("error listing order: %w", err)
+	}
+
+	for i := range orders {
+		var items []OrderItem
+		err = ms.db.SelectContext(ctx, &items, "SELECT * FROM order_items WHERE order_id=?", orders[i].ID)
+
+		if err != nil {
+			return nil, fmt.Errorf("error getting order items: %w", err)
+		}
+
+		orders[i].Items = items
+	}
+
+	return orders, nil
+}
+
+func (ms *MySQLStorer) DeleteOrder(ctx context.Context, id int64) error {
+	err := ms.execTx(ctx, func(tx *sqlx.Tx) error {
+		_, err := tx.ExecContext(ctx, "DELETE FROM order_items WHERE order_id=?", id)
+
+		if err != nil {
+			return fmt.Errorf("error deleting order items: %w", err)
+		}
+
+		_, err = tx.ExecContext(ctx, "DELETE FROM orders WHERE id=?", id)
+
+		if err != nil {
+			return fmt.Errorf("error deleting order %w", err)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("error deleting order: %w", err)
+	}
+
+	return nil
+}
+
+func (ms *MySQLStorer) execTx(ctx context.Context, fn func(*sqlx.Tx) error) error {
+	tx, err := ms.db.BeginTxx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("error starting transaction: %w", err)
+	}
+	err = fn(tx)
+
+	if err != nil {
+		// on error, try to rollback
+		if rbErr := tx.Rollback(); rbErr != nil {
+			// if the rollback failed then show why rollback failed
+			return fmt.Errorf("error rolling back transaction: %w", rbErr)
+		}
+		// else if the rollback didnt fail, show why transaction failed
+		return fmt.Errorf("error in transaction: %w", err)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("error committing transcation %w", err)
 	}
 
 	return nil
--- ecomm-api/storer/types.go
+++ ecomm-api/storer/types.go
@@ -13,5 +13,46 @@ type Product struct {
 	Price        float32    `db:"price"`
 	CountInStock int64      `db:"count_in_stock"`
 	CreatedAt    time.Time  `db:"created_at"`
-	UpdatedAt    *time.Time `db:"updated_at"` //HAS POINTER BECASE ITS NULLABLE
+	UpdatedAt    *time.Time `db:"updated_at"`
+}
+
+type OrderStatus string
+
+const (
+	Pending   OrderStatus = "pending"
+	Shipped   OrderStatus = "shipped"
+	Delivered OrderStatus = "delivered"
+)
+
+type Order struct {
+	ID            int64       `db:"id"`
+	PaymentMethod string      `db:"payment_method"`
+	TaxPrice      float32     `db:"tax_price"`
+	ShippingPrice float32     `db:"shipping_price"`
+	TotalPrice    float32     `db:"total_price"`
+	UserID        int64       `db:"user_id"`
+	Status        OrderStatus `db:"status"`
+	CreatedAt     time.Time   `db:"created_at"`
+	UpdatedAt     *time.Time  `db:"updated_at"`
+	Items         []OrderItem
+}
+
+type OrderItem struct {
+	ID        int64   `db:"id"`
+	Name      string  `db:"name"`
+	Quantity  int64   `db:"quantity"`
+	Image     string  `db:"image"`
+	Price     float32 `db:"price"`
+	ProductID int64   `db:"product_id"`
+	OrderID   int64   `db:"order_id"`
+}
+
+type User struct {
+	ID        int64      `db:"id"`
+	Name      string     `db:"name"`
+	Email     string     `db:"email"`
+	Password  string     `db:"password"`
+	IsAdmin   bool       `db:"is_admin"`
+	CreatedAt time.Time  `db:"created_at"`
+	UpdatedAt *time.Time `db:"updated_at"`
 }
